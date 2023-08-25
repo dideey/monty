3edb7a5218f34eea1fe738381d457ab9907b22eb
dideey <bradleyjd32@gmail.com>
2023-08-25 07:26:04 -0700

    error

--- monty.h
+++ monty.h
@@ -31,4 +31,6 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void pall(stack_t **stack, unsigned int line_number);
+void custom_push(stack_t **series, unsigned int line);
 #endif
