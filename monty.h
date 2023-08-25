5b7b66aa6c8d30d541077c1cc5b8739121090dfe
dideey <bradleyjd32@gmail.com>
2023-08-25 08:07:06 -0700

    er

--- monty.h
+++ monty.h
@@ -1,4 +1,4 @@
-#ifdef monty_h
+#ifndef monty_h
 #define monty_h
 
 /**
@@ -12,9 +12,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -27,8 +27,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 void pall(stack_t **stack, unsigned int line_number);
