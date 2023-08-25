8f3644b642cadb77c28699e02cad9b223748c33e
dideey <bradleyjd32@gmail.com>
2023-08-25 07:21:27 -0700

    error

--- pall.c
+++ pall.c
@@ -4,19 +4,20 @@
 /**
  * pall - prints all elements of the stack
  * @stack: data structure
- * @line_number: no use 
- * Return: nothing 
+ * @line_number: no use
+ * Return: nothing
  */
 
 void pall(stack_t **stack, unsigned int line_number)
 {
-stack_t *i;
-UNUSED(line_number);
-i = NULL;
-while(i != NULL)
-{
-printf("%d\n", i->n)
-i = i->next;
-}
+	stack_t *i;
+
+	UNUSED(line_number);
+	i = NULL;
+	while (i != NULL)
+	{
+		printf("%d\n", i->n)
+			i = i->next;
+	}
 
 }
